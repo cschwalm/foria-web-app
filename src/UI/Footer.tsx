bdcf2cbd92328a948c9ee63777c03f38dffc5a02
Billy Stetson <billy@foriatickets.com>
2020-06-11 12:22:10 -0700

    FWA-104: Sign Up Page UI
    - Initial signup screen formatting

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -26,54 +26,55 @@ const Footer = (byLayout: <A, B>(a: A, b: B) => A | B) => {
     }
 
     return (
-        <div
-            style={{
-                flex: 1,
-                flexDirection: "column",
-                display: "flex",
-                maxWidth: `${BODY_WIDTH}px`,
-                backgroundColor: antiFlashWhite,
-                position: "relative",
-                margin: "0 auto"
-            }}>
-            <div
-                className="column"
-                style={{padding: "0 4px", margin: byLayout("1em", "2em 1.5em")}}>
-                <div className="row" style={{marginBottom: "0.6em"}}>
-                    <a
-                        href={LINK_FORIA_PRIVACY}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={styles.footerLink}>
-                        Privacy Policy
-                    </a>
-                    <span
-                        style={{
-                            color: trolleyGray,
-                            fontFamily: "Roboto",
-                            margin: "0 0.4em"
-                        }}>
-              |
-            </span>
-                    <a
-                        href={LINK_FORIA_TERMS}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={styles.footerLink}>
-                        Terms of Use
-                    </a>
-                </div>
-                <div style={styles.copyright}>
-                    &copy; 2020 Foria Technologies, Inc. All Rights Reserved.
-                </div>
-            </div>
+        <div style={{backgroundColor: antiFlashWhite}}>
             <div
                 style={{
-                    // Create an empty rectangle the size of the collapsed pull up menu,
-                    // so that the footer is not hidden beneath the menu
-                    height: `${5 * FONT_3}px`
-                }}
-            />
+                    flex: 1,
+                    flexDirection: "column",
+                    display: "flex",
+                    maxWidth: `${BODY_WIDTH}px`,
+                    position: "relative",
+                    margin: "0 auto"
+                }}>
+                <div
+                    className="column"
+                    style={{padding: "0 4px", margin: byLayout("1em", "2em 1.5em")}}>
+                    <div className="row" style={{marginBottom: "0.6em"}}>
+                        <a
+                            href={LINK_FORIA_PRIVACY}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={styles.footerLink}>
+                            Privacy Policy
+                        </a>
+                        <span
+                            style={{
+                                color: trolleyGray,
+                                fontFamily: "Roboto",
+                                margin: "0 0.4em"
+                            }}>
+              |
+            </span>
+                        <a
+                            href={LINK_FORIA_TERMS}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={styles.footerLink}>
+                            Terms of Use
+                        </a>
+                    </div>
+                    <div style={styles.copyright}>
+                        &copy; 2020 Foria Technologies, Inc. All Rights Reserved.
+                    </div>
+                </div>
+                <div
+                    style={{
+                        // Create an empty rectangle the size of the collapsed pull up menu,
+                        // so that the footer is not hidden beneath the menu
+                        height: `${5 * FONT_3}px`
+                    }}
+                />
+            </div>
         </div>
     );
 };
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -4,6 +4,8 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {AppState} from "../redux/store";
 import {byLayout as byLayoutWrapper} from "../layout";
+import {BUTTON_HEIGHT, BUTTON_WIDTH} from "../utils/constants";
+import {vividRaspberry, white} from "../utils/colors";
 
 interface SignUpScreenProps {
     byLayout: <A, B>(a: A, b: B) => A | B;
@@ -13,8 +15,42 @@ class SignUpScreen extends Component<SignUpScreenProps> {
 
     render () {
 
+        const buttonStyle = {
+            cursor: "pointer",
+            height: BUTTON_HEIGHT,
+            width: BUTTON_WIDTH,
+            flex: `0 0 ${BUTTON_HEIGHT}`,
+            backgroundColor: vividRaspberry,
+            borderRadius: "5px",
+            color: white,
+            fontWeight: 500,
+            justifyContent: "center",
+            alignItems: "center",
+            margin: '0 auto'
+        }
+
         const renderBody = (
-            <p>test</p>
+            <div
+                style={{
+                    height: '100%',
+                    background: `url(https://foriatickets.com/img/background.jpg) no-repeat`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    alignItems: 'center'
+                }}
+            >
+                <div style={{margin: '0 10%'}}>
+                    <p style={{textAlign: 'center'}}>Discover amazing events. Access personalized discounts.</p>
+                    <p>Foria curates events based on your music taste and enables organizers to offer
+                        personalized discounts to the people they want in their crowd</p>
+                    <div
+                        style={buttonStyle}
+                    >
+                        Join The Foria Family
+                    </div>
+                </div>
+
+            </div>
         );
 
         return (
@@ -22,7 +58,8 @@ class SignUpScreen extends Component<SignUpScreenProps> {
                 {NavBar(this.props.byLayout)}
                 {renderBody}
                 {Footer(this.props.byLayout)}
-            </>    );
+            </>
+        );
     }
 }
 
